fef4c66cea77b602d8f9905214081868cd9841e0
Luisa <luisa.stueckelberger@gmail.com>
2020-04-06 11:05:40 +0200

    Use different users for admin / not admin

--- model/DBAccess.php
+++ model/DBAccess.php
@@ -6,19 +6,14 @@ class DBAccess
     private static $dbAccess;
 
     //Connection
-    private $connection;
     private $adminConnection;
+    private $connection;
     private const user = 'root';
     private const password = '';
     private const adminUser = 'root'; //TODO
     private const adminPassword = ''; //TODO
     private const host = 'localhost';
     private const database = 'lyricsweb';
-
-    private function __construct(){
-        $this->openConnection();
-        $this->openAdminConnection();
-    }
 
     public static function getController(){
         if(is_null(self::$dbAccess) ){
@@ -28,106 +23,105 @@ class DBAccess
     }
 
     public function getConnection(){
-        return $this->connection;
+        return $this->getConn();
     }
+
+    private function getConn(){
+        try {
+            $this->connection = (LoginController::isAdmin()) ? new mysqli($this::host,$this::adminUser,$this::adminPassword,$this::database) : new mysqli($this::host,$this::user,$this::password,$this::database);
+            return $this->connection;
+        }
+        catch(PDOException $e){
+            echo '<p>Verbindung fehlgeschlagen';
+            if(ini_get('display_errors')){
+                echo $e -> getMessage();
+            }
+            exit;
+        }
+    }
+
 
     public function getSongtext(Genre $genre, $songId){
         $query = "select * from lyrics where id = ".$songId." and fk_genre = ".$genre->getId() ;
+        $return = false;
         try {
-            $result = mysqli_query($this->connection, $query);
+            $result = mysqli_query($this->getConn(), $query);
             if (!is_null($result)){
                 $row = mysqli_fetch_array($result);
-                return new Songtext($row['id'],$row['songtext'],$row['titel'],$genre);
-            } else return false;
+                $return = new Songtext($row['id'],$row['songtext'],$row['titel'],$genre);
+            }
+
         } catch (Exception $e) {
             echo "Query konnte nicht ausgeführt werden";
-            return false;
         }
+        return $return;
     }
 
     public function getGenre($id){
         $query = "select * from genre where id = ".$id ;
+        $return = false;
         try {
-            $result = mysqli_query($this->connection, $query);
+            $result = mysqli_query($this->getConn(), $query);
             if (!is_null($result)){
                 $row = mysqli_fetch_array($result);
                 $genre = new Genre($row['id'],$row['genre']);
                 $genre->setSongs($this->getAllSongs($genre));
-                return $genre;
-            } else return false;
+                $return = $genre;
+            }
+
         } catch (Exception $e) {
             echo "Query konnte nicht ausgeführt werden";
-            return false;
         }
+        return $return;
     }
 
     public function getUser($username){
         $query = "select * from `login` where username like '$username'";
+        $return = false;
         try {
-            $result = mysqli_query($this->connection, $query);
+            $result = mysqli_query($this->getConn(), $query);
             if ($result){
                 $row = mysqli_fetch_array($result);
-                return new User($row['username'],$row['password'], $row['admin']);
-            } else return false;
+                $return = new User($row['username'],$row['password'], $row['admin']);
+            }
+
         } catch (Exception $e) {
             var_dump($e);
             echo "Query konnte nicht ausgeführt werden";
-            return false;
         }
+        return $return;
     }
 
     public function insertUser(User $user){
         $query = "insert into login (username, password, admin) values  (?, ?, ?);";
+        $return = false;
         try {
-            if ($stmt = mysqli_prepare($this->connection, $query)) {
+            if ($stmt = mysqli_prepare($this->getConn(), $query)) {
                 $username = $user->getUsername();
                 $hashedPassword = $user->getHashedPassword();
                 $isAdmin = $user->isAdmin();
                 mysqli_stmt_bind_param($stmt, "ssb", $username, $hashedPassword, $isAdmin);
                 mysqli_stmt_execute($stmt);
                 mysqli_stmt_close($stmt);
-                return true;
-            } else return false;
+                $return = true;
+            }
+
         } catch (Exception $e) {
             echo "Query konnte nicht ausgeführt werden";
             return false;
         }
+        return $return;
     }
 
     public function updateSongtext(Songtext $songtext){
-
+        return true;
     }
-
-    public function openAdminConnection(){
-        try {
-            $this->adminConnection = new mysqli($this::host,$this::adminUser,$this::adminPassword,$this::database);
-        }
-        catch(PDOException $e){
-            echo '<p>Verbindung fehlgeschlagen';
-            if(ini_get('display_errors')){
-                echo $e -> getMessage();
-            }
-            exit;
-        }
-    }
-    public function openConnection(){
-        try {
-            $this->connection = new mysqli($this::host,$this::user,$this::password,$this::database);
-        }
-        catch(PDOException $e){
-            echo '<p>Verbindung fehlgeschlagen';
-            if(ini_get('display_errors')){
-                echo $e -> getMessage();
-            }
-            exit;
-        }
-    }
-
 
     public function loadAllGenres(){
         $query = "select * from genre";
+        $genres = false;
         try {
-            $result = mysqli_query($this->connection, $query);
+            $result = mysqli_query($this->getConn(), $query);
             if (!is_null($result)){
                 $genres = [];
                 while($row = mysqli_fetch_array($result)){
@@ -135,30 +129,31 @@ class DBAccess
                     $genre->setSongs($this->getAllSongs($genre));
                     $genres[] = $genre;
                 }
-                return $genres;
             }
+
         } catch (Exception $e) {
             echo "Query konnte nicht ausgeführt werden";
-            return false;
         }
+        return $genres;
     }
 
 
     private function getAllSongs(Genre $genre){
         $query = "select * from lyrics where fk_genre = ".$genre->getId() ;
+        $songs = false;
         try {
-            $result = mysqli_query($this->connection, $query);
+            $result = mysqli_query($this->getConn(), $query);
             if (!is_null($result)){
                 $songs = [];
                 while($row = mysqli_fetch_array($result)){
                     $songs[] = new Songtext($row['id'],$row['songtext'],$row['titel'],$genre);
                 }
-                return $songs;
             }
+
         } catch (Exception $e) {
             echo "Query konnte nicht ausgeführt werden";
-            return false;
         }
+        return $songs;
     }
 
 
